7d7c438ace6fe7bc0aebea3b4f41d8fbdde486fe
Marcin Stenka <mastenka@wp.pl>
2023-05-09 13:55:50 +0200

    1.6.7 - temporary closed fixed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,40 +3,47 @@ import Covid from '../../assets/hero.png';
 import Lead from '../../assets/lead.png';
 import { Scene } from 'react-scrollmagic';
 const Hero = ({ HeroRef }) => {
-  return (
-    <div className="hero-container" ref={HeroRef}>
-      <Scene duration={850} classToggle={['.nav-1', 'active']}>
-        <div className="hero-logo">
-          <img src={Logo} alt="Logo parku dinozaurów Łeba" />
-        </div>
-      </Scene>
-      <div className="hero-covid">
-        <img
-          src={Covid}
-          alt="Informacje o pracy parku podczas pandemii oraz możliwość zakupienia biletu online"
-        />
+	return (
+		<div className='hero-container' ref={HeroRef}>
+			<Scene duration={850} classToggle={['.nav-1', 'active']}>
+				<div className='hero-logo'>
+					<img src={Logo} alt='Logo parku dinozaurów Łeba' />
+				</div>
+			</Scene>
+			<div className='hero-covid'>
+				<img
+					src={Covid}
+					alt='Informacje o pracy parku podczas pandemii oraz możliwość zakupienia biletu online'
+				/>
 
-        <h2 className="covid-button">KUP BILET ONLINE</h2>
-      </div>
-      <div className="hero-lead">
-        <p className='lead-info bold'>Uwaga!<br/>W związku z przebudową drogi i związane z tym utrudnienia od 24.04 - 27.04.2023 Park będzie nieczynny.</p>
-        <p className="lead-info">
-          Wizyta u nas to niezwykła, fantastyczna przygoda w przeszłość, aż do
-          epoki kredy. Tylko w Łebie w Parku Dinozaurów spotkasz
-          trójwymiarowe gady naturalnej wielkości.
-        </p>
-        <div className="lead-img">
-          <img src={Lead} alt="Trasa pełna przygód!" />
-        </div>
-        <p className="lead-info">
-          Pełna przygód i atrakcji trasa, niezapomniane widoki i przeżycia,
-          które na zawsze pozostaną w pamięci małego człowieka. Fantastyczna
-          przygoda edukacyjna dla małych i doskonała zabawa dla dużych
-          miłośników największych gadów w historii.
-        </p>
-      </div>
-    </div>
-  );
+				<h2 className='covid-button'>KUP BILET ONLINE</h2>
+			</div>
+			<div className='hero-lead'>
+				<p className='lead-info bold'>
+					Uwaga!
+					<br />
+					W związku z przebudową drogi i związane z tym utrudnienia od 24.04 -
+					27.04.2023 Park będzie nieczynny.
+					<br />
+					Zapraszamy od 28.04.2023
+				</p>
+				<p className='lead-info'>
+					Wizyta u nas to niezwykła, fantastyczna przygoda w przeszłość, aż do
+					epoki kredy. Tylko w Łebie w Parku Dinozaurów spotkasz trójwymiarowe
+					gady naturalnej wielkości.
+				</p>
+				<div className='lead-img'>
+					<img src={Lead} alt='Trasa pełna przygód!' />
+				</div>
+				<p className='lead-info'>
+					Pełna przygód i atrakcji trasa, niezapomniane widoki i przeżycia,
+					które na zawsze pozostaną w pamięci małego człowieka. Fantastyczna
+					przygoda edukacyjna dla małych i doskonała zabawa dla dużych
+					miłośników największych gadów w historii.
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Hero;
